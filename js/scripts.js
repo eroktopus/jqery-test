a5c8d5e835ec62d3708e33daf88188ac7c2153cb
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-01-31 14:01:21 -0500

    jqery
    added 2 items to list, changed the order of list, deleted item from list, crossed out items on list

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let list = $('#list');
+$('#list').find('li:eq(3)').insertBefore('li:eq(0)');
+list.addClass("strike");
+list.append ("<li>Bauhaus</li>");
+list.append ("<li>Gang of Four</li>");
+
+let band = $('#band');
+band.addClass("delete");
+
+
+
+
+
+
+  
